0f2d098d2fbce9d230a519e76341dcad590ccdad
IsabelGrandison <103618679+IsabelGrandison@users.noreply.github.com>
2022-04-21 15:28:42 -0400

    number of times the word the in a sentence

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,23 +1,20 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class main {
         public static void main(String[] args) {
-                String str1 = "The length of last word";
-                System.out.println("Original String: "+str1);
-                System.out.println("Length of the last word of the above string: "+length_Of_last_word(str1));
-        }
-
-        public static int length_Of_last_word(String str1) {
-                int length_word = 0;
-                String[] words = str1.split(" ");
-                if(words.length>0) {
-                        length_word = words[words.length-1].length();
-                } else {
-                        length_word = 0;
+                int numOfThes = 0;
+                System.out.print("Enter a sentence: ");
+                var scanner = new Scanner(System.in);
+                var inString = scanner.nextLine();
+                var string = new StringTokenizer(inString);
+                while (string.hasMoreTokens()) {
+                        if (string.nextToken().equalsIgnoreCase("the")) {
+                                numOfThes++;
+                        }
                 }
-                return length_word;
-        }
-}
+                System.out.println("Number of Thes: " + numOfThes);
+        }}
 
 
 
