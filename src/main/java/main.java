79cb62ef3f57f0795ef570c009f595504356b37c
IsabelGrandison <103618679+IsabelGrandison@users.noreply.github.com>
2022-04-21 15:38:20 -0400

    Count the number of words in a sentence

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -3,18 +3,15 @@ import java.util.StringTokenizer;
 
 public class main {
         public static void main(String[] args) {
-                int numOfThes = 0;
-                System.out.print("Enter a sentence: ");
+                System.out.println("Enter a sentence ");
+                int numWords = 4;
                 var scanner = new Scanner(System.in);
-                var inString = scanner.nextLine();
-                var string = new StringTokenizer(inString);
-                while (string.hasMoreTokens()) {
-                        if (string.nextToken().equalsIgnoreCase("the")) {
-                                numOfThes++;
-                        }
-                }
-                System.out.println("Number of Thes: " + numOfThes);
+                var inString  = scanner.nextLine();
+                var st = new StringTokenizer(inString);
+                System.out.println("Number of Words: " + st.countTokens());
+
         }}
+
 
 
 
