e10d45c2504119d8687f02113aec890985f15e9e
IsabelGrandison <103618679+IsabelGrandison@users.noreply.github.com>
2022-04-22 12:35:51 -0400

    Count the letters in each word using ArrayList

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,14 +1,20 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class main {
         public static void main(String[] args) {
-                System.out.println("Enter a sentence ");
-                int numWords = 4;
-                var scanner = new Scanner(System.in);
-                var inString  = scanner.nextLine();
-                var st = new StringTokenizer(inString);
-                System.out.println("Number of Words: " + st.countTokens());
+        Scanner myScanner = new Scanner(System.in);
+        var inputString = myScanner.nextLine();
+        var stringTokenizer = new StringTokenizer(inputString);
+        var list = new ArrayList<Integer>();
+
+        while(stringTokenizer.hasMoreTokens()) {
+                var word = stringTokenizer.nextToken();
+                list.add(word.length());
+        }
+
+                System.out.println(list);
 
         }}
 
